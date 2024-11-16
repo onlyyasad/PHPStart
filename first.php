88468ad37b971ddf600c2f538aef53ab20c41b08
Asad <hafij.asad@moveon.com.bd>
2024-11-16 13:20:47 +0600

    first commit

---
+++ first.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>First PHP Code to Learn</title>
+    </head>
+    <body>
+        <marquee behavior="" direction="">Hello PHP</marquee>
+        <?php 
+            echo "Hello World";
+
+            $a = 20;
+            echo "First variable value is: ". $a;
+        ?>
+    </body>
+</html>
