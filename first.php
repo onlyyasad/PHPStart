b12f659e34eac30fadebbc7aa24d83190de786b8
Asad <hafij.asad@moveon.com.bd>
2024-11-16 13:28:52 +0600

    first addition with php

--- first.php
+++ first.php
@@ -8,7 +8,11 @@
             echo "Hello World";
 
             $a = 20;
+            $b = 13;
+            $c = $a + $b;
             echo "First variable value is: ". $a;
+            echo "<br>";
+            echo "Addition of value ".$a." and value ".$b." is ".$c;
         ?>
     </body>
 </html>
